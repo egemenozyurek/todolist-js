614c20b95e6854277352da2568bbf76ee75db910
egemenozyurek <ozyurekegemen95@gmail.com>
2022-03-03 16:53:27 +0300

    looking much better :)

--- js/script.js
+++ js/script.js
@@ -1,75 +1,57 @@
-const TEXTDOM = document.getElementById("task");
-const ULDOM = document.querySelector("ul#list");
+// HTML SAYFASINDAKİ ELEMENTLERİN SEÇİLMESİ
 
-let mainList = new Array();
-//Is there any defined local storage?
-if (localStorage.getItem("todolist") == null) {
-  //Crate local storage with empty array JSON.stringify using save data to localstorage as object array
-  localStorage.setItem("todolist", JSON.stringify(mainList));
-} else {
-  //pull data drom localstorage JSON.parse using pull object data from localstorage to local variable
-  mainList = JSON.parse(localStorage.getItem("todolist"));
-  //Show saved list
-  showList();
+let btnDOM = document.querySelector("#liveToastBtn"); // liveToastBtn id'si ile ekle yazan butona seçtik ve bu butonu btnDOM değişkenine atadık.
+let listDOM = document.querySelector("#list"); // list id'si ile ul'nin ID'si olan list'i seçtik ve listDOM'a atadık.
+let taskDOM = document.querySelector("#task"); //input'un ID'si olan task'ı seçip taskDOM'a atadık.
+let ullength = document.getElementsByTagName("li"); //burda var olan bütün li elementlerini alıp ullength'e atadık böylece elimizde kaç madde yani li olduğunu öğrendik.
+
+// MEVCUT LİSTEYİ SİLMEK İÇİN ÇARPI BUTONUNUN OLUŞTURULMASI
+for (let i = 0; i < ullength.length; i++) {
+  let closeButton = document.createElement("span"); //close icon'u span etiketi içersinde olduğu için yeni bir span elemanı oluşturup ve closeButton değişkenine atadık.
+  closeButton.textContent = "\u00D7"; // listede çarpı işaretini oluşturabilmek için "\u00D7" kullandık.
+  closeButton.classList.add("close"); // bu butona close class'ını ekledik.
+  closeButton.onclick = removeButton; // ve çarpı işaretine basınca removeButton fonsiyonunu çalıştırması gerektini söyledik.
+  ullength[i].append(closeButton); // closeButton değişkenini ullength'in 0, 1, 2... indexlerine ekleyerek aslında for döngüsünü kullanarak var olan listeye çarpı butonunu ekledik.
+  ullength[i].onclick = check; // üzerine tıklayınca check fonksiyonunu çalıştır dedik.
 }
 
-//Button on click func
-function newElement() {
-  if (isInvalid(TEXTDOM.value)) {
-    addToList(TEXTDOM.value);
+// Butonlara dinleyici tanımlanması
+
+btnDOM.addEventListener("click", elemanEkle); // addEventListener ile "click" dediğimiz için butona tıklandığında  elemanEkle fonksiyonu çalışacak.
+
+//Fonksiyonlar
+
+function check() {
+  this.classList.toggle("checked"); // toggle switch genelde iki şıklı (evet, hayır veya aktif pasif) gibi durumları belirtmek için kullanılır. burda toggle("checked")'i kullanarak tıklanan maddenin üstünü çiz ve yanına tik işareti koy demiş olduk.
+}
+
+function removeButton() {
+  this.parentElement.remove(); // burda maddeyi silmek için yanı çarpının bulunduğu maddeyi silmek için parentElement.remove classını kullandık.
+}
+
+//ELEMAN EKLEMEK İÇİN GEREKENLER
+function elemanEkle() {
+  if (taskDOM.value == "") {
+    // input değeri boş girildiğinde ve girilmediğinde
+    $(".error").toast("show"); //error clasını kullanarak
   } else {
-    $(document).ready(function () {
-      $("#attentionToast").toast("show");
-    });
-    TEXTDOM.value = "";
-  }
-}
-function addToList(taskName) {
-  //Add to list
-  mainList.push(String(taskName));
-  console.log(mainList);
-  //Save array to localstorage
-  localStorage.setItem("todolist", JSON.stringify(mainList));
-  //Show task list
-  showList();
-  //Push toast
-  $(document).ready(function () {
-    $("#successToast").toast("show");
-  });
-  //Clear the textbox
-  TEXTDOM.value = "";
-}
-function deleteItem(index) {
-  mainList.splice(index, 1);
-  localStorage.setItem("todolist", JSON.stringify(mainList));
-  showList();
-  //Push toast
-  $(document).ready(function () {
-    $("#deleteItemToast").toast("show");
-  });
-}
-function showList() {
-  //Clear list
-  ULDOM.innerHTML = "";
-  //task is a name belongs we added as string form
-  //key is a number depends index in array
-  mainList.forEach((task, key) => {
-    //Create Li Element
-    let liDOM = document.createElement("li");
-    //Done Button Added
-    liDOM.addEventListener("click", taskDone);
-    //Standardized list element
-    liDOM.innerHTML = `${task}<span id="closeButton" class="deleteButton" onclick=deleteItem(${key})><i class="fas fa-trash"></i></span>`;
+    $(".success").toast("show");
 
-    //Add Element to UL
-    ULDOM.append(liDOM);
-  });
-}
-function isInvalid(task) {
-  let res = task.trim() ? true : false;
-  return res;
-}
-function taskDone() {
-  //Change with boostrap succes style
-  this.classList.toggle("bg-success");
+    let liDOM = document.createElement("li"); //yeni bir li elementi yaratacagımızı ilan edip yaratacagımız li elementini liDOM değişkenine atayacağımızı söyledik.
+    listDOM.appendChild(liDOM); // Yaratacağımız liDOM değişkeninin her seferinde mevcut listenin en sonuna eklenmesi gerektiğini tanımladık.
+    liDOM.innerHTML = task.value; // Burda ise inputID.değer diyerek inputa girilen değerlerin liDOM'a atanması gerektiğini belirttik.
+    taskDOM.value = "";
+
+    //SONRADAN EKLENEN MADDELERİ SİLMEK İÇİN AYNI İŞLEMLERİN TEKRARI
+    liDOM.onclick = check;
+
+    let closeButton = document.createElement("span");
+    closeButton.textContent = "\u00D7";
+    closeButton.classList.add("close");
+    closeButton.onclick = removeButton;
+
+    liDOM.append(closeButton);
+    listDOM.append(liDOM);
+    inputElement.value = "";
+  }
 }
